2ef283e7ac5e14779209b9dc96ded563a4edb17f
Cambob03 <117651848+Cambob03@users.noreply.github.com>
2026-06-17 14:37:49 +0100

    ActiveRagdoll

--- Assets/Scripts/PhysicsAnimator.cs
+++ Assets/Scripts/PhysicsAnimator.cs
@@ -1,16 +1,56 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PhysicsAnimator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform[] _bones;
+    private Dictionary<Transform, ConfigurableJoint> _configurableJoints = new Dictionary<Transform, ConfigurableJoint>();
+    private Dictionary<Transform, Quaternion> _initialRotations = new Dictionary<Transform, Quaternion>();
+    private Dictionary<Transform, Quaternion> _preAnimationRotations = new Dictionary<Transform, Quaternion>();
+    private Dictionary<Transform, Vector3> _preAnimationPositions = new Dictionary<Transform, Vector3>();
+    private Dictionary<Transform, Quaternion> _animatedRotations = new Dictionary<Transform, Quaternion>();
+
     void Start()
     {
-        
+        _bones = transform.GetComponentsInChildren<Transform>();
+
+        for (var i = 0; i < _bones.Length; i++)
+        {
+            if (_bones[i].TryGetComponent(out ConfigurableJoint configurableJoint))
+                _configurableJoints[_bones[i]] = configurableJoint;
+                _initialRotations[_bones[i]] = _bones[i].localRotation;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //stores physics rotations without animation
+        for (var i = 0; i < _bones.Length; i++)
+        {
+            _preAnimationRotations[_bones[i]] = _bones[i].localRotation;
+            _preAnimationPositions[_bones[i]] = _bones[i].position;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        //stores animated rotation without physics
+        for (var i = 0; i < _bones.Length; i++)
+        {
+            _animatedRotations[_bones[i]] = _bones[i].localRotation;
+        }
+
+        //reverts rotations and positions to physics rotation and positions without animation
+        for (var i = 0; i < _bones.Length; i++)
+        {
+            _bones[i].localRotation = _preAnimationRotations[_bones[i]];
+            _bones[i].position = _preAnimationPositions[_bones[i]];
+        }
+
+        //apply animation to character joints
+        foreach (Transform bone in _configurableJoints.Keys)
+        {
+            _configurableJoints[bone].SetTargetRotationLocal(_animatedRotations[bone], startLocalRotation: _initialRotations[bone]);
+        }
     }
 }
