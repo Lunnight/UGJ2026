6538ce293d14b815aacc7062a87704cfeca43eb0
Lunnight <lunnight5102@gmail.com>
2026-06-18 18:00:53 +0100

    Player Grappling
    Players can now grapple one another if they get within a certain range. This stops all movement and disables grappling.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,20 +8,46 @@ public class PlayerController : MonoBehaviour
     public KeyCode MoveRight;
     public bool IsMoveDisabled;
     public float Speed;
+    public bool IsGrappleEnabled;
+    public KeyCode GrappleButton;
+    public GameObject OtherPlayer;
+
+    private bool HasWonGrapple;
+
     void Start()
     {
         IsMoveDisabled = false;
+        IsGrappleEnabled = true;
+        HasWonGrapple = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {  
         if (!IsMoveDisabled)
         {
             if (Input.GetKey(MoveLeft))
                 GetComponent<Rigidbody>().linearVelocity = new Vector3(-Speed, 0, 0);
             if (Input.GetKey(MoveRight))
                 GetComponent<Rigidbody>().linearVelocity = new Vector3(Speed, 0, 0);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (IsGrappleEnabled && collision.collider.name == OtherPlayer.name && collision.thisCollider.name == name)
+        {
+            if (Input.GetKeyDown(GrappleButton))
+            {
+                IsMoveDisabled = true;
+                IsGrappleEnabled = false;
+                HasWonGrapple = true;
+
+                OtherPlayer.GetComponent<PlayerController>().IsMoveDisabled = true;
+                OtherPlayer.GetComponent<PlayerController>().IsGrappleEnabled = false;
+
+                // Trigger minigames
+            }
         }
     }
 }
